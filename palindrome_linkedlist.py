561db91236366f10a332f91351209a66d44349bb
Lokesh Nalla <lokeshnalla2@gmail.com>
2026-07-27 11:44:03 +0530

    solved palindrome linkedlist

---
+++ palindrome_linkedlist.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        arr = []
+
+        while head:
+            arr.append(head.val)
+            head = head.next
+
+        return arr == arr[::-1]
